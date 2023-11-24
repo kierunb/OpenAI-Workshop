489d36bd34556891a3ddc0a7756a14f4b15d51e2
Bartek <kierunb@gmail.com>
2023-11-24 12:58:04 +0100

    fixconfig

--- OpenAIWorkshop.WebAPI/Program.cs
+++ OpenAIWorkshop.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.SemanticKernel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddTransient<IKernel>((serviceProvider) =>
+{
+    return new KernelBuilder()
+    .WithLoggerFactory(serviceProvider.GetRequiredService<ILoggerFactory>())
+    .WithAzureTextCompletionService(
+                builder.Configuration["AzureTextCompletionService:ModelId"]!,
+                builder.Configuration["AzureTextCompletionService:Endpoint"]!,
+                builder.Configuration["AzureTextCompletionService:Key"]!)
+    .WithAzureOpenAIChatCompletionService(
+            builder.Configuration["AzureChatCompletionService:ModelId"]!,
+            builder.Configuration["AzureChatCompletionService:Endpoint"]!,
+            builder.Configuration["AzureChatCompletionService:Key"]!)
+    .Build();
+});
+
 
 var app = builder.Build();
 
